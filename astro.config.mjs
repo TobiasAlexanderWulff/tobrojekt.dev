5480724ebd69bdbf267fee9aa8a028a2a4b95679
Tobias Alexander Wulff <tobias.wulff@live.de>
2025-09-15 02:25:18 +0200

    add Tetris project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import { fileURLToPath } from 'url';
 
 // https://docs.astro.build
 export default defineConfig({
   site: 'https://tobrojekt.dev',
   integrations: [mdx(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '~': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
 });
-
